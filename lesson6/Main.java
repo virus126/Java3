a24e7f7c5773429153a32e0222f36fb659b5aaf7
virus126 <virus126@mail.ru>
2017-06-13 14:31:40 +0300

    Added homework 6, version 1

---
+++ lesson6/Main.java
@@ -0,0 +1,54 @@
+//  1.  Написать метод, которому в качестве аргумента передается не пустой одномерный целочисленный массив, метод должен вернуть новый массив,
+//      который получен путем вытаскивания элементов из исходного массива, идущих после последней четверки. Входной массив должен содержать
+//      хотя бы одну четверку, в противном случае в методе необходимо выбросить RuntimeException.
+//      Написать набор тестов для этого метода (варианта 3-4 входных данных)
+//      вх: [ 1 2 4 4 2 3 4 1 7 ] -> вых: [ 1 7 ]
+//  2.  Написать метод, который проверяет что массив состоит только из чисел 1 и 4. Если в массиве нет хоть одной 4 или 1, то метод вернет false;
+//      Написать набор тестов для этого метода (варианта 3-4 входных данных)
+
+package lesson6;
+
+public class Main {
+    int[] task1(int[] input) {
+        int[] output;
+        int index = -1;
+        int length = 0;
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] == 4) {
+                index = i;
+                length = 0;
+            } else
+                length++;
+        }
+        if (index == -1)
+            throw new RuntimeException();
+        output = new int[length];
+        System.arraycopy(input, ++index, output, 0, length);
+        return output;
+    }
+
+    boolean task2(int[] input) {
+        boolean isThere1 = false;
+        boolean isThere4 = false;
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] == 1) {
+                isThere1 = true;
+                continue;
+            }
+            if (input[i] == 4) {
+                isThere4 = true;
+                continue;
+            }
+            return false;
+        }
+        return isThere1 && isThere4;
+    }
+
+//    public static void main(String[] args) {
+//        Main main = new Main();
+//        int[] arr = main.task1(new int[] { 1, 2, 4, 4, 2, 3, 4, 1, 7});
+//        for (int i = 0; i < arr.length; i++) {
+//            System.out.println(arr[i]);
+//        }
+//    }
+}
---
+++ lesson6/MainTest.java
@@ -0,0 +1,48 @@
+package lesson6;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+    Main main = new Main();
+
+    @Test
+    public void test1_1() {
+        Assert.assertArrayEquals(new int[] {1, 7}, main.task1(new int[] { 1, 2, 4, 4, 2, 3, 4, 1, 7}));
+    }
+
+    @Test
+    public void test1_2() {
+        Assert.assertArrayEquals(new int[] {2, 3, 7}, main.task1(new int[] { 4, 2, 3, 7}));
+    }
+
+    @Test
+    public void test1_3() {
+        Assert.assertArrayEquals(new int[] {}, main.task1(new int[] { 1, 2, 2, 3, 1, 7, 4}));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void test1_4() {
+        Assert.assertArrayEquals(new int[] {}, main.task1(new int[] { 1, 2, 2, 3, 1, 7}));
+    }
+
+    @Test
+    public void test2_1() {
+        Assert.assertTrue(main.task2(new int[] {1, 1, 4, 4, 4, 1}));
+    }
+
+    @Test
+    public void test2_2() {
+        Assert.assertTrue(main.task2(new int[] {1, 4, 1, 4, 4}));
+    }
+
+    @Test
+    public void test2_3() {
+        Assert.assertTrue(main.task2(new int[] {1, 2, 3, 1}));
+    }
+
+    @Test
+    public void test2_4() {
+        Assert.assertTrue(main.task2(new int[] {1, 2, 4, 3}));
+    }
+}
